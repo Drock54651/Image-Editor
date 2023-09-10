eeae7b80adf860ebe6e3a0fd524656355b25c769
Derick Sayavong <dsayavong54651@gmail.com>
2023-09-10 16:54:20 -0600

    tab frames good

--- menu.py
+++ menu.py
@@ -11,3 +11,18 @@ class Menu(ctk.CTkTabview):
         self.add('Color')
         self.add('Effect')
         self.add('Export')
+
+        #* WIDGETS
+        PositionFrame(self.tab('Position')) #! attaches the frame to the position tab when clicked on
+        ColorFrame(self.tab('Color')) #! attaches the frame to the position tab when clicked on
+
+
+class PositionFrame(ctk.CTkFrame):
+    def __init__(self, parent):
+        super().__init__(parent, fg_color = 'transparent')
+        self.pack(expand = True, fill = 'both')
+
+class ColorFrame(ctk.CTkFrame):
+    def __init__(self, parent):
+        super().__init__(parent, fg_color = 'transparent')
+        self.pack(expand = True, fill = 'both')
